6a258797bf51a614d3c8ecece86f18c1bcaede82
ilmposter <97394212+ilmposter@users.noreply.github.com>
2024-07-02 14:22:41 +0300

    Commit changes

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,18 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Алексей", "Сидоров", (byte) 25);
+        userService.saveUser("Виктор", "Прокопьев", (byte) 29);
+        userService.saveUser("Алишер", "Морозов", (byte) 34);
+        userService.saveUser("Евгений", "Гальцев", (byte) 18);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
         // реализуйте алгоритм здесь
     }
 }
