0aa2f47fde3b7ba149315b173e0476a345dff20f
ilmposter <97394212+ilmposter@users.noreply.github.com>
2024-07-04 00:31:34 +0300

    commit HibernateUtil dev

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,8 +1,14 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Environment;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     public static void print(String message) {
@@ -34,5 +40,40 @@ public class Util {
             print("Отключение невозможно!");
         }
     }
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/sidelnikov_DB");
+                settings.put(Environment.USER, "root");
+                settings.put(Environment.PASS, "root");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+
+                // Optional settings
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+                settings.put(Environment.HBM2DDL_AUTO, "update");
+
+                configuration.setProperties(settings);
+
+                configuration.addAnnotatedClass(User.class);
+
+                sessionFactory = configuration.buildSessionFactory();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                if (sessionFactory != null) {
+                    sessionFactory.close();
+                }
+            }
+        }
+        return sessionFactory;
+    }
 }
-    // реализуйте настройку соеденения с БД
+// реализуйте настройку соеденения с БД
